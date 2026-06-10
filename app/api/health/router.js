a07e6dbbda3878af84687cbcd56ffcbad983ad66
ahmadvip1pm-eng <ahmadvip1pm@gmail.com>
2026-06-10 08:51:50 +0700

    Delete app/api/health/router.js

--- app/api/health/router.js
+++
@@ -1,9 +0,0 @@
-export async function GET() {
-  return Response.json({
-    status: 'online',
-    website: 'running',
-    api: 'active',
-    message: 'Website Next.js berjalan normal di Vercel.',
-    time: new Date().toISOString(),
-  });
-}
